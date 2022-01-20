5b314938648730636a774fd5707179ac9b17398c
AmosTLOU <didala2009@163.com>
2022-01-19 21:34:36 -0700

    Player's building added & Tiny change on control

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,6 +6,8 @@ public class CameraControl : MonoBehaviour
 {
     public float maxOffset_pos_x;
     public float maxOffset_pos_y;
+    public float speed_zoom;
+    public float speed_rotate_xy;
     public float speed_rotate_z;
 
     Vector3 m_camRot;
@@ -29,21 +31,21 @@ public class CameraControl : MonoBehaviour
     {
         float offset_r_y = Input.GetAxis("Mouse X");
         float offset_r_x = Input.GetAxis("Mouse Y");
-        float offset_r_z = Input.GetAxis("Mouse ScrollWheel");
-        m_camRot.x -= offset_r_x;
-        m_camRot.y += offset_r_y;
-        m_camRot.z -= speed_rotate_z * offset_r_z;
+        float offset_r_z = Input.GetAxis("Fire1");
+        offset_r_z -= Input.GetAxis("Fire2");
+        m_camRot.x -= speed_rotate_xy * offset_r_x * Time.deltaTime;
+        m_camRot.y += speed_rotate_xy * offset_r_y * Time.deltaTime;
+        m_camRot.z -= speed_rotate_z * offset_r_z * Time.deltaTime;
 
         float offset_pos_x = Input.GetAxis("Horizontal");
         float offset_pos_y = Input.GetAxis("Vertical");
-        m_camPos.x += offset_pos_x;
+        m_camPos.x += offset_pos_x * Time.deltaTime;
         m_camPos.x = Mathf.Clamp(m_camPos.x, m_camInitialPosX - maxOffset_pos_x, m_camInitialPosX + maxOffset_pos_x);
-        m_camPos.y += offset_pos_y;
+        m_camPos.y += offset_pos_y * Time.deltaTime;
         m_camPos.y = Mathf.Clamp(m_camPos.y, m_camInitialPosY - maxOffset_pos_y, m_camInitialPosY + maxOffset_pos_y);
 
-        float offset_zoom = Input.GetAxis("Fire1");
-        offset_zoom += -1 * Input.GetAxis("Fire2");
-        m_camFOV -= offset_zoom;
+        float offset_zoom = Input.GetAxis("Mouse ScrollWheel");
+        m_camFOV -= speed_zoom * offset_zoom * Time.deltaTime; ;
         m_camFOV = Mathf.Clamp(m_camFOV, 10f, m_camInitialFOV);
 
         transform.eulerAngles = m_camRot;
