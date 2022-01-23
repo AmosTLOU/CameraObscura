a09aa829ab9e39f4864d29d23fc4107c1b9d599c
AmosTLOU <didala2009@163.com>
2022-01-23 14:08:50 -0700

    Improve Capture

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,45 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum GameStatus
+{
+    Shoot,
+    Shooting,
+    Gallery,
+}
+
 public class GameManager : MonoBehaviour
 {
     public Canvas CanvasShoot;
     public Canvas CanvasGallery;
     public PhotoGallery InstancePhotoGallery;
     public GameEvent CameraCaptureEvent;
+    public float TimeCapture;
+    public AudioSource AudioPlayer;
+    public AudioClip SFXClick;
+    public UnityEngine.Video.VideoPlayer VideoPlayer;
 
-    // Temporarily, 0 shoot, 1 gallery
-    int _gameStatus; 
+    Camera _mainCamera;
+    GameStatus _gameStatus;
+    float _lastCaptureTime;
+    bool _justTaken;
+    
 
-    // Start is called before the first frame update
     void Start()
     {
-        _gameStatus = 0;
+        _gameStatus = GameStatus.Shoot;
+        _lastCaptureTime = float.NegativeInfinity;
+        _justTaken = false;
+        _mainCamera = Camera.main;
+        VideoPlayer.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (_gameStatus == 0)
+        if (_gameStatus == GameStatus.Shoot)
         {
             if (Input.GetKeyDown(KeyCode.P))
             {
-                _gameStatus = 1;
+                _gameStatus = GameStatus.Gallery;
                 InstancePhotoGallery.EnterGallery();
                 CanvasShoot.gameObject.SetActive(false);
                 CanvasGallery.gameObject.SetActive(true);
             }
             else if (Input.GetKeyDown(KeyCode.Space))
             {
-                InstancePhotoGallery.Capture();
-                CameraCaptureEvent.Raise();
+                if (TimeCapture + _lastCaptureTime < Time.time)
+                {
+                    _gameStatus = GameStatus.Shooting;
+                    CanvasShoot.gameObject.SetActive(false);
+                    _lastCaptureTime = Time.time;
+                }                
             }
         }
-        else if(_gameStatus == 1)
+        else if(_gameStatus == GameStatus.Shooting)
+        {
+            if (!_justTaken)
+            {
+                AudioPlayer.clip = SFXClick;
+                AudioPlayer.Play();
+                VideoPlayer.gameObject.SetActive(true);
+                //VideoPlayer.Play();
+                InstancePhotoGallery.Capture();
+                CameraCaptureEvent.Raise();
+                _justTaken = true;
+            }
+            if(TimeCapture + _lastCaptureTime < Time.time)
+            {
+                _gameStatus = GameStatus.Shoot;
+                _justTaken = false;
+                VideoPlayer.gameObject.SetActive(false);
+                CanvasShoot.gameObject.SetActive(true);
+            }
+        }
+        // Gallery
+        else if(_gameStatus == GameStatus.Gallery)
         {
             if (Input.GetKeyDown(KeyCode.P))
             {
-                _gameStatus = 0;
+                _gameStatus = GameStatus.Shoot;
                 CanvasShoot.gameObject.SetActive(true);
                 CanvasGallery.gameObject.SetActive(false);
             }
@@ -52,8 +93,6 @@ public class GameManager : MonoBehaviour
             {
                 InstancePhotoGallery.ShowNextPhoto();
             }
-        } 
-        
-        
+        }
     }
 }
--- Assets/Scripts/PhotoGallery.cs
+++ Assets/Scripts/PhotoGallery.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Windows;
+using TMPro;
 
 [HideInInspector]
 public class DuLNode
@@ -24,60 +25,56 @@ public class PhotoGallery : MonoBehaviour
 {
     public RawImage DisplayImage;
 
-    DuLNode headNode;
-    DuLNode tailNode;
-    DuLNode curNode;
-    int cntPhoto;
-    string pathPhotos;
+    DuLNode _headNode;
+    DuLNode _tailNode;
+    DuLNode _curNode;
+    int _cntPhoto;
+    string _pathPhotos;
 
     private void Start()
     {
-        headNode = null;
-        tailNode = null;
-        cntPhoto = 0;
+        _headNode = null;
+        _tailNode = null;
+        _cntPhoto = 0;
 
-        pathPhotos = Application.dataPath + "/SavedFiles/Photos/";
-        if (Directory.Exists(pathPhotos)) 
+        _pathPhotos = Application.dataPath + "/SavedFiles/Photos/";
+        if (Directory.Exists(_pathPhotos)) 
         {
             Debug.Log("Clearing Old Photos.");
-            Directory.Delete(pathPhotos); 
+            Directory.Delete(_pathPhotos); 
         }
         Debug.Log("Creating a new gallery.");
-        Directory.CreateDirectory(pathPhotos);
+        Directory.CreateDirectory(_pathPhotos);
     }
 
     public void Capture()
     {
-        string strDateAndTime = System.DateTime.Now.ToString();
-        ScreenCapture.CaptureScreenshot(pathPhotos + cntPhoto + ".png");
-        if (headNode == null)
+        //string strDateAndTime = System.DateTime.Now.ToString();
+        ScreenCapture.CaptureScreenshot(_pathPhotos + _cntPhoto + ".png");
+        if (_headNode == null)
         {
-            headNode = new DuLNode(strDateAndTime, cntPhoto);
-            tailNode = headNode;
-            headNode.next = tailNode;
-            tailNode.prev = headNode;
+            _headNode = new DuLNode(_cntPhoto.ToString(), _cntPhoto);
+            _tailNode = _headNode;
+            _headNode.next = _tailNode;
+            _tailNode.prev = _headNode;
         }
         else
         {
-            tailNode.next = new DuLNode(strDateAndTime, cntPhoto);
-            tailNode.next.prev = tailNode;
-            tailNode = tailNode.next;
-            tailNode.next = headNode;
-            headNode.prev = tailNode;
+            _tailNode.next = new DuLNode(_cntPhoto.ToString(), _cntPhoto);
+            _tailNode.next.prev = _tailNode;
+            _tailNode = _tailNode.next;
+            _tailNode.next = _headNode;
+            _headNode.prev = _tailNode;
         }
-        cntPhoto++;
+        _cntPhoto++;
     }
 
     public void Show(DuLNode node)
     {
-        if (node == null)
-        {
-
-        }
-        else
+        if (node != null)
         {
             byte[] bytes;
-            bytes = System.IO.File.ReadAllBytes(pathPhotos + node.index + ".png");
+            bytes = System.IO.File.ReadAllBytes(_pathPhotos + node.strName + ".png");
             Texture2D textureLoad = new Texture2D(1, 1);
             textureLoad.LoadImage(bytes);
             if (textureLoad)
@@ -89,29 +86,30 @@ public class PhotoGallery : MonoBehaviour
             {
                 Debug.Log("Load Picture Failure");
             }
+
         }
     }
 
     public void EnterGallery()
     {
-        curNode = headNode;
-        Show(curNode);
+        _curNode = _headNode;
+        Show(_curNode);
     }
 
     public void ShowNextPhoto()
     {
-        if (curNode == null)
+        if (_curNode == null)
             return;
-        Show(curNode.next);
-        curNode = curNode.next;
+        Show(_curNode.next);
+        _curNode = _curNode.next;
     }
 
     public void ShowPrevPhoto()
     {
-        if (curNode == null)
+        if (_curNode == null)
             return;
-        Show(curNode.prev);
-        curNode = curNode.prev;
+        Show(_curNode.prev);
+        _curNode = _curNode.prev;
     }
 
 
