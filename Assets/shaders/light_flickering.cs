6314bcb47d16a7f8c1ff79b04568785a231dc727
AmosTLOU <didala2009@163.com>
2022-02-01 10:55:40 -0700

    Integrate lighting and staging

---
+++ Assets/shaders/light_flickering.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light_flickering : MonoBehaviour
+{
+    public bool isflickering = false;
+    public float timedelay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isflickering == false)
+        {
+            StartCoroutine(lightflicker());
+        }
+    }
+
+    IEnumerator lightflicker()
+    {
+        isflickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timedelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timedelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timedelay = Random.Range(0.01f, 5f);
+        yield return new WaitForSeconds(timedelay);
+        isflickering = false;
+    }
+}
